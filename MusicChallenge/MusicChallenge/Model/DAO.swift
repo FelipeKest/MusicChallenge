25c76cce5e9c7894c5408be2643c273f68c374fd
Felipe Procaci Kestelman <fpkest@gmail.com>
2018-12-10 14:10:54 -0200

    Adição de MVC e Criação de um DAO Singleton

---
+++ MusicChallenge/MusicChallenge/Model/DAO.swift
@@ -0,0 +1,18 @@
+//
+//  DAO.swift
+//  MusicChallenge
+//
+//  Created by Felipe Kestelman on 08/12/18.
+//  Copyright © 2018 Felipe Kestelman. All rights reserved.
+//
+
+import Foundation
+import CloudKit
+
+let DAO = dao.instance
+
+class dao{
+    static let instance = dao()
+   
+    
+}
