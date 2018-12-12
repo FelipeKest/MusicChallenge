1f1421e546fa20517e869e73f35ef14524d25155
Guilherme Vassallo <guilhermevassallo09@gmail.com>
2018-12-12 13:23:17 -0200

    Criando Roadmap View Controller

---
+++ MusicChallenge/MusicChallenge/Controller/RoadmapViewController.swift
@@ -0,0 +1,43 @@
+//
+//  RoadmapViewController.swift
+//  MusicChallenge
+//
+//  Created by Guilherme Vassallo on 12/12/18.
+//  Copyright © 2018 Felipe Kestelman. All rights reserved.
+//
+
+import UIKit
+
+class RoadmapViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    @IBOutlet var roadmapTableView: UITableView!
+    
+    //@IBOutlet var monthSelector: ?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+
+}
