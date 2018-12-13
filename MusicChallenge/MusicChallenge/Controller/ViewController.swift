edcdd8cb8507b9b25b0b7fb24df4738f090e354e
Lucas Gouveia <lucasgouveiagtr@gmail.com>
2018-12-13 17:15:07 -0200

    Segmented Controller funcionando (Falta fazer as telas) - Ajuste das constraints da foto da launch screen.

--- MusicChallenge/MusicChallenge/Controller/ViewController.swift
+++ MusicChallenge/MusicChallenge/Controller/ViewController.swift
@@ -9,11 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet var vwView: UIView!
+    
+    @IBOutlet weak var scSegment: UISegmentedControl!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
+    @IBAction func scSegmentedTapped(_ sender: Any) {
+        
+        let getIndex = scSegment.selectedSegmentIndex
+        print (getIndex)
+        
+        switch (getIndex) {
+        case 0:
+            self.vwView.backgroundColor = UIColor.green
+        case 1:
+            self.vwView.backgroundColor = UIColor.blue
+        default:
+            print("no select")
+        }
+    }
+    
+    
 
 
 }
