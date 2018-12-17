ca5863ae2208867c8af0f0bdd2ff6207a69e1668
Guilherme Vassallo <guilhermevassallo09@gmail.com>
2018-12-17 17:22:11 -0200

    JoinBandViewController criado

---
+++ MusicChallenge/MusicChallenge/Controller/Initial Screens/JoinBandViewController.swift
@@ -0,0 +1,33 @@
+//
+//  JoinBandViewController.swift
+//  MusicChallenge
+//
+//  Created by Guilherme Vassallo on 17/12/18.
+//  Copyright © 2018 Felipe Kestelman. All rights reserved.
+//
+
+import UIKit
+
+class JoinBandViewController: UIViewController {
+    
+    @IBOutlet var bandCodeTextField: UITextField!
+    @IBOutlet var createBandButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
