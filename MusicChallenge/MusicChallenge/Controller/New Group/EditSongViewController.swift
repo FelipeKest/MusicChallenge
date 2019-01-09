fb1a65ccdf547178ea02e63b6b5632d2758b0c19
Guilherme Vassallo <guilhermevassallo09@gmail.com>
2019-01-09 14:56:56 -0200

    Criando telas de editar música e setlist e outras coisas

---
+++ MusicChallenge/MusicChallenge/Controller/New Group/EditSongViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EditSongViewController.swift
+//  MusicChallenge
+//
+//  Created by Guilherme Vassallo on 09/01/19.
+//  Copyright © 2019 Felipe Kestelman. All rights reserved.
+//
+
+import UIKit
+
+class EditSongViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
