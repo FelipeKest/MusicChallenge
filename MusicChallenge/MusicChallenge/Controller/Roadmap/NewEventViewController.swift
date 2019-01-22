946ab99ec7165e2a700b03c3b31aebb3754e35a1
Guilherme Vassallo <guilhermevassallo09@gmail.com>
2019-01-22 15:08:25 -0200

    Novo evento integrado com calendário do iPhone (falta editar)

--- MusicChallenge/MusicChallenge/Controller/Roadmap/NewEventViewController.swift
+++ MusicChallenge/MusicChallenge/Controller/Roadmap/NewEventViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import EventKit
 
 protocol NewEventProtocol {
     func getEvent (newEvent: Event)
@@ -72,16 +73,34 @@ class NewEventViewController: UIViewController , SelectSetlistProtocol{
     func getSetlist(selectedSetlist: Setlist) {
         self.selectedSetlist = selectedSetlist
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    
+    func addEventToCalendar(title: String, description: String?, startDate: Date, endDate: Date, completion: ((_ success: Bool, _ error: NSError?) -> Void)? = nil) {
+        DispatchQueue.global(qos: .background).async { () -> Void in
+            let eventStore = EKEventStore()
+            
+            eventStore.requestAccess(to: .event, completion: { (granted, error) in
+                if (granted) && (error == nil) {
+                    let event = EKEvent(eventStore: eventStore)
+                    event.title = title
+                    event.startDate = startDate
+                    event.endDate = endDate
+                    event.notes = description
+                    event.calendar = eventStore.defaultCalendarForNewEvents
+                    do {
+                        try eventStore.save(event, span: .thisEvent)
+                    } catch let e as NSError {
+                        completion?(false, e)
+                        return
+                    }
+                    completion?(true, nil)
+                } else {
+                    completion?(false, error as NSError?)
+                }
+            })
+        }
     }
-    */
+    
     
     @IBAction func addSetlistButton(_ sender: Any) {
         print("button pressed")
@@ -141,6 +160,8 @@ class NewEventViewController: UIViewController , SelectSetlistProtocol{
             newEvent?.associatedSetlist = selectedSetlist
             
             delegate?.getEvent(newEvent: newEvent!)
+                
+                addEventToCalendar(title: newEvent?.name ?? "ERROR", description: "\(newEvent?.eventType.text ?? "ERROR") em \(newEvent?.location ?? "ERROR")", startDate: newEvent?.date ?? Date(), endDate: newEvent?.date ?? Date())
             
             dismiss(animated: true, completion: nil)
         }
--- MusicChallenge/MusicChallenge/Controller/Roadmap/RoadmapViewController.swift
+++ MusicChallenge/MusicChallenge/Controller/Roadmap/RoadmapViewController.swift
@@ -10,6 +10,7 @@
 
 import UIKit
 
+
 class RoadmapViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, NewEventProtocol {
     
     
