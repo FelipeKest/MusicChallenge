8fb8245b43d18ccc8dc6e3c69329489ae3ec9a16
Guilherme Vassallo <guilhermevassallo09@gmail.com>
2019-01-24 16:15:54 -0200

    criando coisas p/ tableview de membros da banda

--- MusicChallenge/MusicChallenge/Controller/New Group1/Info/BandInfoViewController.swift
+++ MusicChallenge/MusicChallenge/Controller/New Group1/Info/BandInfoViewController.swift
@@ -10,13 +10,39 @@
 
 import UIKit
 
-class BandInfoViewController: UIViewController {
+class BandInfoViewController: UIViewController , UITableViewDataSource, UITabBarDelegate{
+    
 
+    @IBOutlet var membersTableView: UITableView!
+    
+    var band = Band(name: "Developers of Sound", members:[
+        
+        Musician(name: "Guilherme Vassallo", age: 21, band: Band(), id: "asfakgoakga"),
+        Musician(name: "Felipe Kestelmann", age: 19, band: Band(), id: "asfakgoakga"),
+        Musician(name: "Lucas Gouveia", age: 22, band: Band(), id: "asfakgoakga")
+],
+                    
+        id: "agjagiadjgiadg")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return band.members.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let membersCell = tableView.dequeueReusableCell(withIdentifier: "membersCell", for: indexPath) as! MembersTableViewCell
+        
+        
+        
+        return membersCell
+    }
+    
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
